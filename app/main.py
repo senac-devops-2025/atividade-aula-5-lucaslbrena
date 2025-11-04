e89c434d64791ab33700a7c8256175045654b6e8
Lucas Brena <lucaslbrena@gmail.com>
2025-11-04 16:36:36 -0300

    Atividade Aula 5 - estrutura final validada

--- app/main.py
+++ app/main.py
@@ -1,2 +1,3 @@
 def greet(name="DevOps"):
+    """Retorna uma saudação personalizada conforme instrução da Aula 05."""
     return f"Olá, {name}! Bem-vindo à Aula 05."
--- tests/test_main.py
+++ tests/test_main.py
@@ -2,4 +2,5 @@ from app.main import greet
 
 
 def test_greet():
+    """Testa se a função greet retorna a saudação esperada."""
     assert greet() == "Olá, DevOps! Bem-vindo à Aula 05."
